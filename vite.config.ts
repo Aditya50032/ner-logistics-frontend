5e6d2ea86da2bcc5e9b6b06492be1d6f4c0ce9b9
Aditya <adityapathak4004@gmail.com>
2026-09-07 19:28:47 +0530

    Fix vite config conflict

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-<<<<<<< HEAD
-=======
+
+
   base: "/ner-logistics-frontend/",
->>>>>>> 25066e1f9fa6d7bd91c3e7f0bedd40e2d314b6ca
+
   plugins: [react()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
